93a2dd357dc1215b9a38ea088cdba33d4915aff5
vighzhen <vighzhen@vighzhendeMacBook-Air.local>
2024-06-30 17:56:10 +0800

    fix

---
+++ src/main/java/com/vighzhen/fashionista/FashionistaApplication.java
@@ -0,0 +1,13 @@
+package com.vighzhen.fashionista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionistaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vighzhen/fashionista/FashionistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vighzhen.fashionista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionistaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
